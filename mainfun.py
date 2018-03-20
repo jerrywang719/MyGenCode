8ae5779b7bc2a2f603443073a8aba7d600c7d45e
w3c <w3c@w3cschool.cn>
2018-03-21 06:09:39 +0800

    my django

--- mainfun.py
+++ mainfun.py
@@ -1,3 +1,14 @@
 __author__ = 'ping'
-x = 1
-print(x)
+from django.conf import settings
+settings.configure()
+
+from django.template import Context, Template
+s = '''{% if num <= 100 and num >= 0 %}
+num在0到100之间
+{% else %}
+数值不在范围之内！
+{% endif %}'''
+t = Template(s)
+c = Context({'num': 30})
+str = t.render(c)
+print(str)

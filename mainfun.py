9f7a76b3aae50f7e5c77da875e1e8520ea0e24e0
w3c <w3c@w3cschool.cn>
2018-03-22 23:19:55 +0800

    my django

--- mainfun.py
+++ mainfun.py
@@ -2,11 +2,20 @@ __author__ = 'ping'
 from django.conf import settings
 
 settings.configure()
-import tcode
+# import tcode
 from django.template import Context, Template
 
-t = Template(tcode.ts)
-c = Context(tcode.tc)
+try:
+    f = open(r'H:\aa\tempcode.txt', 'r')
+    exec(f.read())
+except:
+    print('文件操作失败！')
+finally:
+    if f:
+        f.close()
+
+t = Template(ts)
+c = Context(tc)
 str = t.render(c)
 print(str)
 try:
